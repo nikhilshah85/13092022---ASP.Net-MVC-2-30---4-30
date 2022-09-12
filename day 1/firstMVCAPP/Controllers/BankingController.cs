7bfe64cebaa1a3f023010692020afd8b74dd18bb
Nikhil <nikhil.shah2008@gmail.com>
2022-09-12 16:13:13 +0530

    Day 1 - MVC Intro

---
+++ day 1/firstMVCAPP/Controllers/BankingController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace firstMVCAPP.Controllers
+{
+    public class BankingController : Controller
+    {
+      
+        public IActionResult BankingHome()
+        {
+            ViewBag.developer = "Nikhil Shah";
+            ViewBag.teamSize = 30;
+            List<string> tList = new List<string>() { ".Net","MVC","SQL Server", "Bootstrap","JQuery","Azure"};
+            ViewBag.techList = tList;
+            ViewBag.isProjectLive = true;
+
+            return View();
+        }
+
+        public IActionResult BankingServices()
+        {
+            return View();
+        }
+
+        public IActionResult TransferFunds()
+        {
+            //collect data from Model to be passed to view - or vice-versa
+            return View();
+        }
+
+
+    }
+}
