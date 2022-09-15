f4d16112fdfc5c7bc52ade45e731a374235de38f
Nikhil <nikhil.shah2008@gmail.com>
2022-09-15 15:50:13 +0530

    EF Core

---
+++ day 4/EF2/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EF2.Models.EF;
+namespace EF2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        MVC_EFContext db = new MVC_EFContext();
+
+        public IActionResult Products()
+        {
+            
+            return View(db.Products.ToList());
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
