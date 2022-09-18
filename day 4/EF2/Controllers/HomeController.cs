e8871e141b6980253197c820b252e27b6d2342b8
Nikhil <nikhil.shah2008@gmail.com>
2022-09-18 15:49:02 +0530

    fetch api

--- day 4/EF2/Controllers/HomeController.cs
+++ day 4/EF2/Controllers/HomeController.cs
@@ -12,7 +12,11 @@ namespace EF2.Controllers
         {
             _logger = logger;
         }
-
+        
+        public IActionResult StorageAPIUse()
+        {
+            return View();
+        }
         public IActionResult Index()
         {
             return View();
---
+++ day 5/webAPI_calls/Controllers/JSONdatacallsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webAPI_calls.Controllers
+{
+    public class JSONdatacallsController : Controller
+    {
+        public IActionResult PostData()
+        {
+            return View();
+        }
+
+        public IActionResult GetCommentsData()
+        {
+            return View();
+        }
+    }
+}
